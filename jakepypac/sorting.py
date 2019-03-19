a8dc87c17702e44fcd439e96bc4e94e9a6c999ee
JakeGitUsername <48679991+JakeGitUsername@users.noreply.github.com>
2019-03-19 17:37:42 +0200

    Update sorting.py

--- jakepypac/sorting.py
+++ jakepypac/sorting.py
@@ -9,17 +9,30 @@ def bubble_sort(items):
     return out
 
 def merge_sort(items):
-
     '''Return array of items, sorted in ascending order'''
     len_i = len(items)
     if len_i == 1:
         return items
 
     mid_point = int(len_i / 2)
-    i1 = merge_sort(items[:mid_point])
-    i2 = merge_sort(items[mid_point:])
+    A = merge_sort(items[:mid_point])
+    B = merge_sort(items[mid_point:])
+    new_list = []
+    while len(A) > 0 and len(B) > 0:
+        if A[0] < B[0]:
+            new_list.append(A[0])
+            A.pop(0)
+        else:
+            new_list.append(B[0])
+            B.pop(0)
 
-    return merge(i1, i2)
+    if len(A) == 0:
+        new_list = new_list + B
+    if len(B) == 0:
+        new_list = new_list + A
+
+    return new_list
+
 
 def quick_sort(items):
 
